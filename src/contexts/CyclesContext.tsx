763b1e0252689e2d8b57b8dc9e3537b1ff008b1c
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-08-27 20:36:22 -0300

    fix: reset

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -67,9 +67,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
         setAmountSecondsPassed(0)
-  
-        // reset()
-     }   
+       }   
   
      function interruptCurrentCycle() {
         setCycles(state => state.map((cycle) => {
