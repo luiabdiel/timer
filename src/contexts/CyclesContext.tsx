dae3d09b29677a70c7b11858fbeceae74c99a49d
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-09-01 12:36:16 -0300

    feat: lib immer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState, useReducer } from "react";
-import { ActionTypes, Cycle, CyclesReducer } from "../reducers/cycles"
+import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { Cycle, CyclesReducer } from "../reducers/cycles/reducer"
 
 interface CreateCycleData {
     task: string
@@ -42,12 +43,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     }
   
     function markCurrentCycleAsFinished() {
-        dispatch({
-            type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-            payload: {
-                activeCycleId,
-            }   
-        })
+        dispatch(markCurrentCycleAsFinishedAction())
     }
     
     function createNewCycle(data: CreateCycleData) {
@@ -60,24 +56,14 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
            startDate: new Date(),
         }
 
-        dispatch({
-            type: ActionTypes.ADD_NEW_CYCLE,
-            payload: {
-                newCycle,
-            }   
-        })
+        dispatch(addNewCycleAction(newCycle))
   
         setAmountSecondsPassed(0)
        }   
   
      function interruptCurrentCycle() {
 
-        dispatch({
-            type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
-            payload: {
-                activeCycleId,
-            }   
-        })
+        dispatch(interruptCurrentCycleAction())
      }
 
     return (
